2ef3bf217f184208d40adb3115358b2a8ea13ca4
newsnick <atlcontact8@gmail.com>
2023-10-02 09:57:08 +0200

    fetch products from react with useEffect

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const products = require('./data/products')
+const products = require('./data/products.js')
 
 const app = express()
 
@@ -7,13 +7,19 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('api/products', (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json(products)
 })
 
-app.get('api/products/:id', (req, res) => {
-  const product = product.fin((p) => p._id === req.params.id)
-  res.json(products)
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404).json({ message: 'Product not found' })
+  }
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+app.listen(5000, () => {
+  console.log('Server running on port 5000')
+})
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
   const { id } = useParams()
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState(null)
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        setProduct(data)
+      } catch (error) {}
+    }
+
+    fetchProduct()
+  }, [id])
+
+  if (!product) {
+    return <div>Loading...</div>
+  }
 
   return (
     <>
