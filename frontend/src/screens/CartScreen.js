8dc83baa3ffde85b5cf07ba39c17afa746a40107
newsnick <atlcontact8@gmail.com>
2023-10-05 10:03:16 +0200

    (upd) add to cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,56 @@
-import React from 'react'
+// import React, { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import Message from '../components/Message'
+// import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+// import { addToCart } from '../actions/cartActions'
 
-const cartScreen = () => {
-  return (
-    <div>Cart</div>
-  )
+// const CartScreen = ({ match, location, history }) => {
+//   console.log(`match data: ${match}`)
+//   const productId = match.params.id
+
+//   const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+//   const dispatch = useDispatch()
+
+//   useEffect(() => {
+//     if (productId) {
+//       dispatch(addToCart(productId, qty))
+//     }
+//   }, [dispatch, productId, qty])
+
+//   return <div>Cart</div>
+// }
+
+// export default CartScreen
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { useParams, useLocation } from 'react-router-dom'
+import { addToCart } from '../actions/cartActions'
+
+const CartScreen = ({ match, history }) => {
+  const { id } = useParams()
+  const productId = id
+
+  // const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  const location = useLocation()
+
+  const qty = new URLSearchParams(location.search).get('qty') || 1
+
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+  console.log(cartItems)
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
+  return <div>Cart</div>
 }
 
-export default cartScreen
+export default CartScreen
