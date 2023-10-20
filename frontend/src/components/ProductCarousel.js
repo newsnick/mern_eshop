748dedd7be961523d82cc2143b60ed6b9ad496ec
newsnick <atlcontact8@gmail.com>
2023-10-20 10:15:57 +0200

    [upd](frontend) modify carousel css

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,3 +1,46 @@
+// import React, { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { Link } from 'react-router-dom'
+// import { Carousel, Image } from 'react-bootstrap'
+// import Loader from './Loader'
+// import Message from './Message'
+// import { listTopProducts } from '../actions/productActions'
+
+// const ProductCarousel = () => {
+//   const dispatch = useDispatch()
+
+//   const productTopRated = useSelector((state) => state.productTopRated)
+//   const { loading, error, products } = productTopRated
+
+//   useEffect(() => {
+//     dispatch(listTopProducts())
+//   }, [dispatch])
+
+//   return loading ? (
+//     <Loader />
+//   ) : error ? (
+//     <Message variant='danger'>{error}</Message>
+//   ) : (
+//     <Carousel pause='hover' className='bg-dark'>
+//       {products.map((product) => (
+//         <Carousel.Item key={product._id}>
+//           <Link to={`/product/${product._id}`}>
+//             <Image src={product.image} alt={product.name} fluid />
+
+//             <Carousel.Caption className='carousel-caption'>
+//               <h2>
+//                 {product.name} (${product.price})
+//               </h2>
+//             </Carousel.Caption>
+//           </Link>
+//         </Carousel.Item>
+//       ))}
+//     </Carousel>
+//   )
+// }
+
+// export default ProductCarousel
+
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -16,26 +59,36 @@ const ProductCarousel = () => {
     dispatch(listTopProducts())
   }, [dispatch])
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant='danger'>{error}</Message>
-  ) : (
-    <Carousel pause='hover' className='bg-dark'>
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+  return (
+    <div className='product-carousel-container'>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Carousel pause='hover' className='bg-dark'>
+          {products.map((product) => (
+            <Carousel.Item key={product._id}>
+              <Link to={`/product/${product._id}`} className='carousel-link'>
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  className='carousel-image'
+                  fluid
+                />
 
-            <Carousel.Caption className='carousel-caption'>
-              <h2>
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+                <Carousel.Caption className='carousel-caption'>
+                  <h2 className='product-name'>
+                    {product.name} (${product.price})
+                  </h2>
+                  <p className='product-description'>{product.description}</p>
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
+    </div>
   )
 }
 
