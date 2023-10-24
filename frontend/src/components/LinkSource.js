d5606865df2b86f780ec7bd52580f51ce8e96497
newsnick <atlcontact8@gmail.com>
2023-10-24 08:18:44 +0200

    [upd](frontend) add new product

--- frontend/src/components/LinkSource.js
+++ frontend/src/components/LinkSource.js
@@ -15,6 +15,15 @@ const LinkSource = () => {
       auf Freepik
       <br />
       <a
+        href='https://www.freepik.com/free-psd/television-mock-up-design_1076463.htm#page=2&query=hdtv%20showcase&position=0&from_view=search&track=ais'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Image by zlatko_plamenov
+      </a>{' '}
+      on Freepik
+      <br />
+      <a
         href='https://de.freepik.com/fotos-kostenlos/glaenzende-schwarze-kopfhoerer-reflektieren-goldene-nachtclublichter-die-von-ki-erzeugt-werden_40970146.htm#query=jbl%20tune&position=19&from_view=search&track=ais'
         target='_blank'
         rel='noopener noreferrer'
