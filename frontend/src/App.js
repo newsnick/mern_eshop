7bfee19b8ed80fdefc131fee2584ccf36fc20618
newsnick <atlcontact8@gmail.com>
2023-10-27 10:36:48 +0200

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
